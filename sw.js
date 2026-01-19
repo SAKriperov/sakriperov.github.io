9ce3a7141569e034684f1c664e2b9e3812321922
SAKriperov <sakriperov@outlook.com>
2026-01-19 17:17:52 +0800

    Site updated: 2026-01-19 17:17:52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767776876596";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768814266472";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
